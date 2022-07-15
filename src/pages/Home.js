10ce8b83d24a47b95fa9870f069b8ceb70fc1b71
Joey Bonventre <jbonventre95@gmail.com>
2022-07-15 16:57:49 -0400

    home page and routes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,25 @@
+import logo from "../images/NunzioLogo.jpg";
+import { Link } from "react-router-dom";
+
+const linkStyle = {
+  margin: "1rem",
+  textDecoration: "none",
+  color: "white",
+};
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="shop">
+        <img src={logo} alt="logo" />
+        <button className="shop-btn">
+          <Link to="/shop" style={linkStyle}>
+            ENTER SHOP
+          </Link>
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Home;
