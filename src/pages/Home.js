1918d22d7a19e0d29523ce96a34d9ed555881ed6
Joey Bonventre <jbonventre95@gmail.com>
2022-07-17 21:05:08 -0400

    Homepage remodel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
-import logo from "../images/NunzioLogo.jpg";
 import { Link } from "react-router-dom";
+import homeBKG from "../images/home-bkg.jpg";
+import cartLogo from "../images/cart.png";
+import logo from "../images/full-logo.png";
 
 const linkStyle = {
   margin: "1rem",
@@ -11,12 +13,28 @@ const Home = () => {
   return (
     <>
       <div className="shop">
-        <img src={logo} alt="logo" />
-        <button className="shop-btn">
-          <Link to="/shop" style={linkStyle}>
-            ENTER SHOP
-          </Link>
-        </button>
+        <header>
+          <img src={logo} alt="logo" />
+          <nav>
+            <a href="/shop">SHOP</a>
+            <a href="/aboutus">ABOUT US</a>
+            <a href="/contact">CONTACT</a>
+            <img src={cartLogo} alt="cart" />
+          </nav>
+        </header>
+        <img src={homeBKG} alt="kitchen" />
+        <div className="welcome">
+          <p>
+            IN BUSINESS FOR OVER <span>30 YEARS</span> THE NUNZIO & SONS FAMILY
+            WELCOMES ALL NEW AND FAMILIAR FACES TO VIEW OUR IMPRESSIVE VARIETY
+            OF GRANITE AND MARBLE TO MAKE YOUR HOME SPECTACULAR{" "}
+          </p>
+          <button className="shop-btn">
+            <Link to="/shop" style={linkStyle}>
+              ENTER SHOP
+            </Link>
+          </button>
+        </div>
       </div>
     </>
   );
